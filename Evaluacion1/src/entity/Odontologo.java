53f508d97bcd03b514380cf8fdca019ab42c2452
Nahuel Gonzalez <glnahuel17@gmail.com>
2023-05-25 19:57:59 -0300

    Prueba Odontologo

--- Evaluacion1/src/entity/Odontologo.java
+++ Evaluacion1/src/entity/Odontologo.java
@@ -1,4 +1,13 @@
 package entity;
 
 public class Odontologo {
+    private int matricula;
+    private String nombre;
+    private String apellido;
+
+    public Odontologo(int matricula, String nombre, String apellido) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 }
