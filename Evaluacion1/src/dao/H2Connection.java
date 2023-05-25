a82c28d651598d0fb655631dd353a2f815be7a47
Nahuel Gonzalez <glnahuel17@gmail.com>
2023-05-25 20:30:22 -0300

    H2Connection Correccion

--- Evaluacion1/src/dao/H2Connection.java
+++ Evaluacion1/src/dao/H2Connection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class H2Connection {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/c9clase15", "sa", "sa");
+        return DriverManager.getConnection("jdbc:h2:~/Eval1Nahuel", "sa", "sa");
     }
 }
 
