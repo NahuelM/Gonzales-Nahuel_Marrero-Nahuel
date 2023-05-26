8f887836c44beb9a0501a453d49efcce5d4758fb
Nahuel Gonzalez <glnahuel17@gmail.com>
2023-05-25 21:19:48 -0300

    Implementacion OdontologoH2

--- Evaluacion1/src/dao/imp/OdontologoH2.java
+++ Evaluacion1/src/dao/imp/OdontologoH2.java
@@ -1,8 +1,11 @@
 package dao.imp;
 
+import dao.H2Connection;
 import entity.Odontologo;
 import dao.IDao;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -10,11 +13,78 @@ public class OdontologoH2 implements IDao <Odontologo> {
     private static final Logger LOGGER = Logger.getLogger(OdontologoH2.class);
     @Override
     public Odontologo guardar(Odontologo odontologo) {
-        return null;
-    }
+        Connection connection = null;
+        try{
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
 
+
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, odontologo.getMatricula());
+            ps.setString(2, odontologo.getNombre());
+            ps.setString(3, odontologo.getApellido());
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+            while (rs.next()){
+                odontologo.setMatricula(rs.getInt(1));
+            }
+
+            connection.commit();
+            LOGGER.info("Se ha registrado el Odontologo: " + odontologo);
+
+        }catch (Exception e){
+            LOGGER.info(e.getMessage());
+            e.printStackTrace();
+            if(connection != null){
+                try {
+                    connection.rollback();
+                    System.out.println("Tuvimos un problema");
+                    e.printStackTrace();
+                } catch (SQLException exception){
+                    LOGGER.info(exception.getMessage());
+                    exception.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception e){
+                LOGGER.info("Ha ocurrido un error al intentar cerrar la bdd. " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        return odontologo;
+    }
     @Override
     public List<Odontologo> listartodos() {
-        return null;
+
+        Connection connection = null;
+        List<Odontologo> odontologos= new ArrayList<>();
+
+        try {
+            connection = H2Connection.getConnection();
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ODONTOLOGOS");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Odontologo odontologo = new Odontologo(rs.getInt(1), rs.getString(2), rs.getString(3));
+                odontologos.add(odontologo);
+            }
+
+            LOGGER.info("Listado de todos los odontologos: " + odontologos);
+
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
+            e.printStackTrace();
+
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                LOGGER.info("Ha ocurrido un error al intentar cerrar la bdd. " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+        return odontologos;
     }
 }
--- Evaluacion1/src/entity/Odontologo.java
+++ Evaluacion1/src/entity/Odontologo.java
@@ -1,6 +1,30 @@
 package entity;
 
 public class Odontologo {
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     private int matricula;
     private String nombre;
     private String apellido;
